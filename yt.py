2d0134e1fdbf1ab235095f715a32e1d8b09b34fd
Oktay Savcı <93824768+oktaysavci@users.noreply.github.com>
2024-10-30 15:24:05 +0300

    Update yt.py

--- yt.py
+++ yt.py
@@ -5,10 +5,13 @@ video_url = input("Video URL'sini girin: ")
 print("Çözünürlük seçenekleri: \n1. 720p\n2. 480p\n3. 360p\n4. 240p\n5. 144p")
 quality = input("İndirmek istediğiniz kaliteyi seçin (ör. 720p): ")
 
+name = input("Video ismini girin: ")
+
 # İndirilecek kaliteye göre format ayarlarını yapılandır
 ydl_opts = {
     'format': f'bestvideo[height<={quality[:-1]}]+bestaudio/best[height<={quality[:-1]}]',
-    'outtmpl': '%(title)s.%(ext)s'  
+    'outtmpl': f'{name}.%(ext)s',  # Kullanıcının girdiği adı dosya adı olarak ayarla
+    'merge_output_format': 'mp4',  # Çıktıyı mp4 formatında birleştir
 }
 
 # Video indirme işlemi
